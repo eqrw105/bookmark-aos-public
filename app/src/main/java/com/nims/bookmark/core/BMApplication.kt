e511a67307645657e36d9a560f13902d7fd871db
seongmin <green@macciatto.com>
2022-01-24 10:13:03 +0900

    application initFolder fail fix

--- app/src/main/java/com/nims/bookmark/core/BMApplication.kt
+++ app/src/main/java/com/nims/bookmark/core/BMApplication.kt
@@ -6,7 +6,7 @@ import com.nims.bookmark.R
 import com.nims.bookmark.di.apiModule
 import com.nims.bookmark.di.viewModelModule
 import com.nims.bookmark.room.Folder
-import com.nims.bookmark.ui.RegisterRepositoryImpl
+import com.nims.bookmark.ui.RepositoryImpl
 import org.koin.android.ext.android.get
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -30,7 +30,7 @@ class BMApplication : Application() {
      * folder 없을 시 전체 폴더 추가
      */
     private fun initFolder() {
-        val repository: RegisterRepositoryImpl = get()
+        val repository: RepositoryImpl = get()
         if (repository.getFolders().isNullOrEmpty()) {
             val date = Calendar.getInstance().time.time
             val folder = Folder(
